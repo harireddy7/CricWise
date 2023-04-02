baf6ca620e164d7ca9f1281ca20eadae3ca26afd
harireddy7 <harimsd8@gmail.com>
2023-04-02 21:39:52 +0530

    removed unnecessary styles

--- src/Pages/PlayerTabs/PlayerTabs.jsx
+++ src/Pages/PlayerTabs/PlayerTabs.jsx
@@ -12,10 +12,10 @@ const PlayerTabs = () => {
         <CustomTab>Squad 🔢</CustomTab>
       </TabList>
       <TabPanels>
-        <TabPanel px={0} maxW='95vw' minHeight='60vh' overflow='hidden' bg='yellow'>
+        <TabPanel px={0} maxW='95vw' minHeight='60vh' overflow='hidden'>
           <Playing11 />
         </TabPanel>
-        <TabPanel px={0} maxW='95vw' minHeight='60vh' overflow='hidden' bg='yellow'>
+        <TabPanel px={0} maxW='95vw' minHeight='60vh' overflow='hidden'>
           <Squad />
         </TabPanel>
       </TabPanels>
